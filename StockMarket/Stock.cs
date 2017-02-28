b59ad61ce28891e101eb869fca77162692d08a4c
Mark Mendoza <markolo25@gmail.com>
2017-02-28 00:49:12 -0800

    finished creating classes needed

---
+++ StockMarket/Stock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace StockMarket
+{
+    internal class Stock
+    {
+        private string _stockName;
+        private int _initialValue;
+        private int _maximumChange;
+        private int _notificationThreshold;
+        private Thread _thread;
+
+        public Stock(string StockName, int InitialValue, int MaximumChange, int notificationThreshold)
+        {
+            this._stockName = StockName;
+            this._initialValue = InitialValue;
+            this._maximumChange = MaximumChange;
+            this._notificationThreshold = notificationThreshold;
+            this._thread = new Thread(new ThreadStart(Activate));
+        }
+
+        private void Activate()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                ChangeStockValue();
+            }
+        }
+
+        private void ChangeStockValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StockMarket/StockBroker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StockMarket
+{
+    internal class StockBroker
+    {
+        private string v;
+
+        public StockBroker(string v)
+        {
+            this.v = v;
+        }
+
+        internal void AddStock(Stock stock1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
